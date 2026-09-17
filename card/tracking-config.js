7927bd7d7d58c6fcd7fcb4ac090288d25924f9ff
Douglas Leão <doug.leaop@gmail.com>
2026-09-16 22:14:31 -0300

    Configure card tracking endpoint

--- card/tracking-config.js
+++ card/tracking-config.js
@@ -1,3 +1,2 @@
-// Depois de publicar lead-tracker.gs como Aplicação Web, cole aqui a URL /exec.
-// A URL é pública e não é uma chave secreta.
-window.BENCAO_TRACKING_ENDPOINT = '';
+// Endpoint público da Aplicação Web; não é uma chave secreta.
+window.BENCAO_TRACKING_ENDPOINT = 'https://script.google.com/macros/s/AKfycbxVtkFCtpdTrPaemU692Oevgh45AyovVEb6eNIkHGnnnPJJA6i2ZYFdrWez8F3et3uV/exec';
